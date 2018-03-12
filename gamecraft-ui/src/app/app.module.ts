df5e86ae446da60a85daf1683ed908c1b32cb3b3
Iván Martínez Mateu <ivanmartinezmateu@icloud.com>
2018-03-12 22:39:08 +0100

    Add Gamecraft UI skeleton

---
+++ gamecraft-ui/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+
+import { AppComponent } from './app.component';
+import { AppNavbarComponent } from './app-navbar/app-navbar.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AppNavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
