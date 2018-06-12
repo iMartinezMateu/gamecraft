aca3aafa83ea6b34fb9ace55430a31297ad4cc2e
Iván Martínez Mateu <ivanmartinezmateu@icloud.com>
2018-06-12 14:12:04 +0200

    Implemented Team CRUD functions in Gamecraft UI

--- gamecraft-ui/src/main/resources/static/js/teams_operations.js
+++ gamecraft-ui/src/main/resources/static/js/teams_operations.js
@@ -23,18 +23,94 @@ function getTeams() {
     return teamsList;
 }
 
-function searchTeam() {
+function addTeam(teamName, teamDescription) {
+    var queryUrl = location.protocol + '//' + document.domain + ":8080/api/teams/";
 
+    var data = {
+        teamName: teamName,
+        teamDescription: teamDescription,
+    };
+    $.ajax
+    ({
+        type: "POST",
+        url: queryUrl,
+        async: false,
+        data: JSON.stringify(data),
+        contentType: "application/json",
+        beforeSend: function (xhr) {
+            // set header if JWT is set
+            if (sessionStorage.getItem("token")) {
+                xhr.setRequestHeader("Authorization", "Bearer " + sessionStorage.getItem("token"));
+            }
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        success: function (data) {
+            alert("Team created!");
+        },
+        error: function (data) {
+            alert(JSON.stringify(data));
+        }
+    });
 }
 
-function addTeam() {
+function updateTeam(teamId, teamName, teamDescription) {
+    var queryUrl = location.protocol + '//' + document.domain + ":8080/api/teams/";
 
+    var data = {
+        teamId: teamId,
+        teamName: teamName,
+        teamDescription: teamDescription,
+    };
+    $.ajax
+    ({
+        type: "PUT",
+        url: queryUrl,
+        async: false,
+        data: JSON.stringify(data),
+        contentType: "application/json",
+        beforeSend: function (xhr) {
+            // set header if JWT is set
+            if (sessionStorage.getItem("token")) {
+                xhr.setRequestHeader("Authorization", "Bearer " + sessionStorage.getItem("token"));
+            }
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        success: function (data) {
+            alert("Team updated!");
+        },
+        error: function (data) {
+            alert(JSON.stringify(data));
+        }
+    });
 }
 
-function updateTeam() {
+function deleteTeam(teamId) {
+    var queryUrl = location.protocol + '//' + document.domain + ":8080/api/teams/" + teamId;
 
-}
-
-function deleteTeam() {
-
+    $.ajax
+    ({
+        type: "DELETE",
+        url: queryUrl,
+        async: false,
+        contentType: "application/json",
+        beforeSend: function (xhr) {
+            // set header if JWT is set
+            if (sessionStorage.getItem("token")) {
+                xhr.setRequestHeader("Authorization", "Bearer " + sessionStorage.getItem("token"));
+            }
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        success: function (data) {
+            alert("Team deleted!");
+        },
+        error: function (data) {
+            alert(JSON.stringify(data));
+        }
+    });
 }
