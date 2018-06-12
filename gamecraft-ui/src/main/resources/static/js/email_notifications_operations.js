f40741966a410343d8c815d617bd83da1c0e93c9
Iván Martínez Mateu <ivanmartinezmateu@icloud.com>
2018-06-12 16:31:26 +0200

    Add CRUD functions for managing e-mail notificators in Gamecraft UI

--- gamecraft-ui/src/main/resources/static/js/email_notifications_operations.js
+++ gamecraft-ui/src/main/resources/static/js/email_notifications_operations.js
@@ -26,14 +26,106 @@ function getEmailAccounts() {
     return accountList;
 }
 
-function addEmailAccount() {
+function addEmailAccount(emailAccountDescription, emailAccountName, emailSmtpPassword, emailSmtpPort, emailSmtpServer, emailSmtpUseSSL, emailSmtpUsername) {
+    var queryUrl = location.protocol + '//' + document.domain + ":8080/gamecraftemailnotificationmanager/api/email-accounts/";
 
+    var data = {
+        "emailAccountDescription": emailAccountDescription,
+        "emailAccountEnabled": true,
+        "emailAccountName": emailAccountName,
+        "emailSmtpPassword": emailSmtpPassword,
+        "emailSmtpPort": emailSmtpPort,
+        "emailSmtpServer": emailSmtpServer,
+        "emailSmtpUseSSL": emailSmtpUseSSL,
+        "emailSmtpUsername": emailSmtpUsername
+    };
+    $.ajax
+    ({
+        type: "POST",
+        url: queryUrl,
+        async: false,
+        data: JSON.stringify(data),
+        contentType: "application/json",
+        beforeSend: function (xhr) {
+            // set header if JWT is set
+            if (sessionStorage.getItem("token")) {
+                xhr.setRequestHeader("Authorization", "Bearer " + sessionStorage.getItem("token"));
+            }
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        success: function (data) {
+            alert("E-mail notificator created!");
+        },
+        error: function (data) {
+            alert(JSON.stringify(data));
+        }
+    });
 }
 
-function updateEmailAccount() {
+function updateEmailAccount(emailNotificatorId,emailAccountDescription, emailAccountEnabled, emailAccountName, emailSmtpPassword, emailSmtpPort, emailSmtpServer, emailSmtpUseSSL, emailSmtpUsername) {
+    var queryUrl = location.protocol + '//' + document.domain + ":8080/gamecraftemailnotificationmanager/api/email-accounts/";
 
+    var data = {
+        "id": emailNotificatorId,
+        "emailAccountDescription": emailAccountDescription,
+        "emailAccountEnabled": emailAccountEnabled,
+        "emailAccountName": emailAccountName,
+        "emailSmtpPassword": emailSmtpPassword,
+        "emailSmtpPort": emailSmtpPort,
+        "emailSmtpServer": emailSmtpServer,
+        "emailSmtpUseSSL": emailSmtpUseSSL,
+        "emailSmtpUsername": emailSmtpUsername
+    };
+    $.ajax
+    ({
+        type: "PUT",
+        url: queryUrl,
+        async: false,
+        data: JSON.stringify(data),
+        contentType: "application/json",
+        beforeSend: function (xhr) {
+            // set header if JWT is set
+            if (sessionStorage.getItem("token")) {
+                xhr.setRequestHeader("Authorization", "Bearer " + sessionStorage.getItem("token"));
+            }
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        success: function (data) {
+            alert("E-mail notificator updated!");
+        },
+        error: function (data) {
+            alert(JSON.stringify(data));
+        }
+    });
 }
 
-function deleteEmailAccount() {
+function deleteEmailAccount(emailNotificatorId) {
+    var queryUrl = location.protocol + '//' + document.domain + ":8080/gamecraftemailnotificationmanager/api/email-accounts/" + emailNotificatorId;
 
+    $.ajax
+    ({
+        type: "DELETE",
+        url: queryUrl,
+        async: false,
+        contentType: "application/json",
+        beforeSend: function (xhr) {
+            // set header if JWT is set
+            if (sessionStorage.getItem("token")) {
+                xhr.setRequestHeader("Authorization", "Bearer " + sessionStorage.getItem("token"));
+            }
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        success: function (data) {
+            alert("E-mail notificator deleted!");
+        },
+        error: function (data) {
+            alert(JSON.stringify(data));
+        }
+    });
 }
