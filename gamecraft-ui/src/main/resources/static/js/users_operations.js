cce540c74c8dec9707f6ed18c8d7b9ee41fb13cc
Iván Martínez Mateu <ivanmartinezmateu@icloud.com>
2018-06-12 14:03:02 +0200

    Implemented addUser function in Gamecraft UI

--- gamecraft-ui/src/main/resources/static/js/users_operations.js
+++ gamecraft-ui/src/main/resources/static/js/users_operations.js
@@ -30,8 +30,46 @@ function searchUser() {
 
 }
 
-function addUser() {
+function addUser(authorities, author, langKey, email, firstName, lastName, username, password) {
+    var queryUrl = location.protocol + '//' + document.domain + ":8080/api/users/";
 
+    var data = {
+            activated: true,
+            authorities: authorities,
+            createdBy: author,
+            createdDate: new Date().getDate(),
+            langKey: langKey,
+            lastModifiedBy: author,
+            lastModifiedDate: new Date().getDate(),
+            email: email,
+            firstName: firstName,
+            lastName: lastName,
+            login: username,
+            password: password
+        };
+    $.ajax
+    ({
+        type: "POST",
+        url: queryUrl,
+        async: false,
+        data: JSON.stringify(data),
+        contentType: "application/json",
+        beforeSend: function (xhr) {
+            // set header if JWT is set
+            if (sessionStorage.getItem("token")) {
+                xhr.setRequestHeader("Authorization", "Bearer " + sessionStorage.getItem("token"));
+            }
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        success: function (data) {
+            alert("User created!");
+        },
+        error: function (data) {
+            alert(JSON.stringify(data));
+        }
+    });
 }
 
 function updateUser() {
@@ -112,6 +150,28 @@ function updatePersonalInformation(username,password,email,firstname,lastname) {
     changePassword(password);
 }
 
-function deleteUser() {
-
+function deleteUser(login) {
+    var queryUrl = location.protocol + '//' + document.domain + ":8080/api/users/" + login;
+    $.ajax
+    ({
+        type: "DELETE",
+        url: queryUrl,
+        async: false,
+        contentType: "application/json",
+        beforeSend: function (xhr) {
+            // set header if JWT is set
+            if (sessionStorage.getItem("token")) {
+                xhr.setRequestHeader("Authorization", "Bearer " + sessionStorage.getItem("token"));
+            }
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        success: function (data) {
+            alert("User deleted!");
+        },
+        error: function (data) {
+            alert(JSON.stringify(data));
+        }
+    });
 }
