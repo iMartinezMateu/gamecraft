b881aa51665c9a79b3586c7b025eaab7e0103ecc
Iván Martínez Mateu <ivanmartinezmateu@icloud.com>
2018-06-12 16:41:53 +0200

    Add CRUD functions for managing IRC notificators in Gamecraft UI

--- gamecraft-ui/src/main/resources/static/js/irc_notifications_operations.js
+++ gamecraft-ui/src/main/resources/static/js/irc_notifications_operations.js
@@ -26,14 +26,104 @@ function getIRCBots() {
     return botList;
 }
 
-function addIRCBot() {
+function addIRCBot(ircBotName, ircBotDescription, ircBotName, ircBotNickname, ircServerAddress, ircServerPort, ircServerSecuredProtocolEnabled) {
+    var queryUrl = location.protocol + '//' + document.domain + ":8080/gamecraftircnotificationmanager/api/irc-bots/";
 
+    var data = {
+        "ircBotDescription": ircBotDescription,
+        "ircBotEnabled": true,
+        "ircBotName": ircBotName,
+        "ircBotNickname": ircBotNickname,
+        "ircServerAddress": ircServerAddress,
+        "ircServerPort": ircServerPort,
+        "ircServerSecuredProtocolEnabled": ircServerSecuredProtocolEnabled
+    };
+    $.ajax
+    ({
+        type: "POST",
+        url: queryUrl,
+        async: false,
+        data: JSON.stringify(data),
+        contentType: "application/json",
+        beforeSend: function (xhr) {
+            // set header if JWT is set
+            if (sessionStorage.getItem("token")) {
+                xhr.setRequestHeader("Authorization", "Bearer " + sessionStorage.getItem("token"));
+            }
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        success: function (data) {
+            alert("IRC notificator created!");
+        },
+        error: function (data) {
+            alert(JSON.stringify(data));
+        }
+    });
 }
 
-function updateIRCBot() {
+function updateIRCBot(ircBotId, ircBotName, ircBotDescription, ircBotEnabled, ircBotNickname, ircServerAddress, ircServerPort, ircServerSecuredProtocolEnabled) {
+    var queryUrl = location.protocol + '//' + document.domain + ":8080/gamecraftircnotificationmanager/api/irc-bots/";
 
+    var data = {
+        "ircBotId": ircBotId,
+        "ircBotDescription": ircBotDescription,
+        "ircBotEnabled": ircBotEnabled,
+        "ircBotName": ircBotName,
+        "ircBotNickname": ircBotNickname,
+        "ircServerAddress": ircServerAddress,
+        "ircServerPort": ircServerPort,
+        "ircServerSecuredProtocolEnabled": ircServerSecuredProtocolEnabled
+    };
+    $.ajax
+    ({
+        type: "PUT",
+        url: queryUrl,
+        async: false,
+        data: JSON.stringify(data),
+        contentType: "application/json",
+        beforeSend: function (xhr) {
+            // set header if JWT is set
+            if (sessionStorage.getItem("token")) {
+                xhr.setRequestHeader("Authorization", "Bearer " + sessionStorage.getItem("token"));
+            }
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        success: function (data) {
+            alert("IRC notificator created!");
+        },
+        error: function (data) {
+            alert(JSON.stringify(data));
+        }
+    });
 }
 
-function deleteIRCBot() {
+function deleteIRCBot(ircBotId) {
+    var queryUrl = location.protocol + '//' + document.domain + ":8080/gamecraftircnotificationmanager/api/irc-bots/" + ircBotId;
 
+    $.ajax
+    ({
+        type: "DELETE",
+        url: queryUrl,
+        async: false,
+        contentType: "application/json",
+        beforeSend: function (xhr) {
+            // set header if JWT is set
+            if (sessionStorage.getItem("token")) {
+                xhr.setRequestHeader("Authorization", "Bearer " + sessionStorage.getItem("token"));
+            }
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        success: function (data) {
+            alert("IRC notificator deleted!");
+        },
+        error: function (data) {
+            alert(JSON.stringify(data));
+        }
+    });
 }
