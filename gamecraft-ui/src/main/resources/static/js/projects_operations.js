70105cbd7131b8fbb2d0379bb85d6e656ab10e0f
Iván Martínez Mateu <ivanmartinezmateu@icloud.com>
2018-06-12 14:43:28 +0200

    Add CRUD functions for projects manipulation in Gamecraft UI

--- gamecraft-ui/src/main/resources/static/js/projects_operations.js
+++ gamecraft-ui/src/main/resources/static/js/projects_operations.js
@@ -23,18 +23,108 @@ function getProjects() {
     return projectsList;
 }
 
-function searchProject() {
+
+function addProject(projectName, projectDescription, projectWebsite) {
+    var queryUrl = location.protocol + '//' + document.domain + ":8080/gamecraftproject/api/projects/";
+
+    var data = {
+        projectName: projectName,
+        projectDescription: projectDescription,
+        projectWebsite: projectWebsite,
+        projectArchived: false,
+        projectLogo: ""
+    };
+    $.ajax
+    ({
+        type: "POST",
+        url: queryUrl,
+        async: false,
+        data: JSON.stringify(data),
+        contentType: "application/json",
+        beforeSend: function (xhr) {
+            // set header if JWT is set
+            if (sessionStorage.getItem("token")) {
+                xhr.setRequestHeader("Authorization", "Bearer " + sessionStorage.getItem("token"));
+            }
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        success: function (data) {
+            alert("Project created!");
+        },
+        error: function (data) {
+            alert(JSON.stringify(data));
+        }
+    });
+}
+
+function updateProject(projectName, projectDescription, projectWebsite, projectArchived) {
+    var queryUrl = location.protocol + '//' + document.domain + ":8080/gamecraftproject/api/projects/";
+
+    var data = {
+        projectName: projectName,
+        projectDescription: projectDescription,
+        projectWebsite: projectWebsite,
+        projectArchived: projectArchived,
+        projectLogo: ""
+    };
+    $.ajax
+    ({
+        type: "PUT",
+        url: queryUrl,
+        async: false,
+        data: JSON.stringify(data),
+        contentType: "application/json",
+        beforeSend: function (xhr) {
+            // set header if JWT is set
+            if (sessionStorage.getItem("token")) {
+                xhr.setRequestHeader("Authorization", "Bearer " + sessionStorage.getItem("token"));
+            }
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        success: function (data) {
+            alert("Project updated!");
+        },
+        error: function (data) {
+            alert(JSON.stringify(data));
+        }
+    });
+}
+
+function deleteProject(projectId) {
+    var queryUrl = location.protocol + '//' + document.domain + ":8080/gamecraftproject/api/projects/" + projectId;
+
+    $.ajax
+    ({
+        type: "DELETE",
+        url: queryUrl,
+        async: false,
+        contentType: "application/json",
+        beforeSend: function (xhr) {
+            // set header if JWT is set
+            if (sessionStorage.getItem("token")) {
+                xhr.setRequestHeader("Authorization", "Bearer " + sessionStorage.getItem("token"));
+            }
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        success: function (data) {
+            alert("Project deleted!");
+        },
+        error: function (data) {
+            alert(JSON.stringify(data));
+        }
+    });
+}
+
+function assignTeam(teamId, projectId) {
 
 }
 
-function addProject() {
-
-}
-
-function updateProject() {
-
-}
-
-function deleteProject() {
+function deassignTeam(teamId, projectId) {
 
 }
