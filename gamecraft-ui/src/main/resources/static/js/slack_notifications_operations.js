b8edaca8796f6da4cc89e067bced128bf214789f
Iván Martínez Mateu <ivanmartinezmateu@icloud.com>
2018-06-14 09:53:48 +0200

    Add CRUD functions for managing Slack notificators in Gamecraft UI

--- gamecraft-ui/src/main/resources/static/js/slack_notifications_operations.js
+++ gamecraft-ui/src/main/resources/static/js/slack_notifications_operations.js
@@ -26,14 +26,98 @@ function getSlackAccounts() {
     return botList;
 }
 
-function addSlackAccount() {
+function addSlackAccount(slackAccountName,slackAccountDescription,slackAccountToken) {
+    var queryUrl = location.protocol + '//' + document.domain + ":8080/gamecraftslacknotificationmanager/api/slack-accounts/";
 
+    var data = {
+        "slackAccountDescription": slackAccountDescription,
+        "slackAccountEnabled": true,
+        "slackAccountName": slackAccountName,
+        "slackAccountToken": slackAccountToken
+    };
+    $.ajax
+    ({
+        type: "POST",
+        url: queryUrl,
+        async: false,
+        data: JSON.stringify(data),
+        contentType: "application/json",
+        beforeSend: function (xhr) {
+            // set header if JWT is set
+            if (sessionStorage.getItem("token")) {
+                xhr.setRequestHeader("Authorization", "Bearer " + sessionStorage.getItem("token"));
+            }
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        success: function (data) {
+            alert("Slack notificator created!");
+        },
+        error: function (data) {
+            alert(JSON.stringify(data));
+        }
+    });
 }
 
-function updateSlackAccount() {
+function updateSlackAccount(slackAccountId, slackAccountName, slackAccountDescription, slackAccountEnabled, slackAccountToken) {
+    var queryUrl = location.protocol + '//' + document.domain + ":8080/gamecraftslacknotificationmanager/api/slack-accounts/";
 
+    var data = {
+        "id": slackAccountId,
+        "slackAccountDescription": slackAccountDescription,
+        "slackAccountEnabled": slackAccountEnabled,
+        "slackAccountName": slackAccountName,
+        "slackAccountToken": slackAccountToken
+    };
+    $.ajax
+    ({
+        type: "PUT",
+        url: queryUrl,
+        async: false,
+        data: JSON.stringify(data),
+        contentType: "application/json",
+        beforeSend: function (xhr) {
+            // set header if JWT is set
+            if (sessionStorage.getItem("token")) {
+                xhr.setRequestHeader("Authorization", "Bearer " + sessionStorage.getItem("token"));
+            }
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        success: function (data) {
+            alert("Slack notificator updated!");
+        },
+        error: function (data) {
+            alert(JSON.stringify(data));
+        }
+    });
 }
 
-function deleteSlackAccount() {
+function deleteSlackAccount(slackNotificatorId) {
+    var queryUrl = location.protocol + '//' + document.domain + ":8080/gamecraftslacknotificationmanager/api/slack-accounts/" + slackNotificatorId;
 
+    $.ajax
+    ({
+        type: "DELETE",
+        url: queryUrl,
+        async: false,
+        contentType: "application/json",
+        beforeSend: function (xhr) {
+            // set header if JWT is set
+            if (sessionStorage.getItem("token")) {
+                xhr.setRequestHeader("Authorization", "Bearer " + sessionStorage.getItem("token"));
+            }
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        success: function (data) {
+            alert("Slack notificator deleted!");
+        },
+        error: function (data) {
+            alert(JSON.stringify(data));
+        }
+    });
 }
