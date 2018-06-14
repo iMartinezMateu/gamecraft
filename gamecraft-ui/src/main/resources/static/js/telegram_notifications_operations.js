f6e017104a7095494437cb60963d2b22b916aad3
Iván Martínez Mateu <ivanmartinezmateu@icloud.com>
2018-06-14 10:23:41 +0200

    Add CRUD functions for managing Telegram notificators in Gamecraft UI

--- gamecraft-ui/src/main/resources/static/js/telegram_notifications_operations.js
+++ gamecraft-ui/src/main/resources/static/js/telegram_notifications_operations.js
@@ -26,14 +26,98 @@ function getTelegramBots() {
     return botList;
 }
 
-function addTelegramBot() {
+function addTelegramBot(telegramBotName, telegramBotDescription, telegramBotToken) {
+    var queryUrl = location.protocol + '//' + document.domain + ":8080/gamecrafttelegramnotificationmanager/api/telegram-bots/";
 
+    var data = {
+        "telegramBotDescription": telegramBotDescription,
+        "telegramBotEnabled": true,
+        "telegramBotName": telegramBotName,
+        "telegramBotToken": telegramBotToken
+    };
+    $.ajax
+    ({
+        type: "POST",
+        url: queryUrl,
+        async: false,
+        data: JSON.stringify(data),
+        contentType: "application/json",
+        beforeSend: function (xhr) {
+            // set header if JWT is set
+            if (sessionStorage.getItem("token")) {
+                xhr.setRequestHeader("Authorization", "Bearer " + sessionStorage.getItem("token"));
+            }
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        success: function (data) {
+            alert("Telegram notificator created!");
+        },
+        error: function (data) {
+            alert(JSON.stringify(data));
+        }
+    });
 }
 
-function updateTelegramBot() {
+function updateTelegramBot(telegramBotId, telegramBotName, telegramBotDescription, telegramBotEnabled, telegramBotToken) {
+    var queryUrl = location.protocol + '//' + document.domain + ":8080/gamecrafttelegramnotificationmanager/api/telegram-bots/";
 
+    var data = {
+        "id": telegramBotId,
+        "telegramBotDescription": telegramBotDescription,
+        "telegramBotEnabled": telegramBotEnabled,
+        "telegramBotName": telegramBotName,
+        "telegramBotToken": telegramBotToken
+    };
+    $.ajax
+    ({
+        type: "PUT",
+        url: queryUrl,
+        async: false,
+        data: JSON.stringify(data),
+        contentType: "application/json",
+        beforeSend: function (xhr) {
+            // set header if JWT is set
+            if (sessionStorage.getItem("token")) {
+                xhr.setRequestHeader("Authorization", "Bearer " + sessionStorage.getItem("token"));
+            }
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        success: function (data) {
+            alert("Telegram notificator created!");
+        },
+        error: function (data) {
+            alert(JSON.stringify(data));
+        }
+    });
 }
 
-function deleteTelegramBot() {
+function deleteTelegramBot(telegramBotId) {
+    var queryUrl = location.protocol + '//' + document.domain + ":8080/gamecrafttelegramnotificationmanager/api/telegram-bots/" + telegramBotId;
 
+    $.ajax
+    ({
+        type: "DELETE",
+        url: queryUrl,
+        async: false,
+        contentType: "application/json",
+        beforeSend: function (xhr) {
+            // set header if JWT is set
+            if (sessionStorage.getItem("token")) {
+                xhr.setRequestHeader("Authorization", "Bearer " + sessionStorage.getItem("token"));
+            }
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        success: function (data) {
+            alert("Telegram notificator deleted!");
+        },
+        error: function (data) {
+            alert(JSON.stringify(data));
+        }
+    });
 }
