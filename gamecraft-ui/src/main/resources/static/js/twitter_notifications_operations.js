e0b779c2a8260e877b67928c2635739e3cb8161d
Iván Martínez Mateu <ivanmartinezmateu@icloud.com>
2018-06-14 10:44:17 +0200

    Add CRUD functions for managing Twitter notificators in Gamecraft UI

--- gamecraft-ui/src/main/resources/static/js/twitter_notifications_operations.js
+++ gamecraft-ui/src/main/resources/static/js/twitter_notifications_operations.js
@@ -26,14 +26,104 @@ function getTwitterBots() {
     return botList;
 }
 
-function addTwitterBot() {
+function addTwitterBot(twitterBotName, twitterBotDescription, twitterBotConsumerKey, twitterBotConsumerSecret, twitterBotAccessToken, twitterBotAccessTokenSecret) {
+    var queryUrl = location.protocol + '//' + document.domain + ":8080/gamecrafttwitternotificationmanager/api/twitter-bots/";
 
+    var data = {
+        "twitterBotAccessToken": twitterBotAccessToken,
+        "twitterBotAccessTokenSecret": twitterBotAccessTokenSecret,
+        "twitterBotConsumerKey": twitterBotConsumerKey,
+        "twitterBotConsumerSecret": twitterBotConsumerSecret,
+        "twitterBotDescription": twitterBotDescription,
+        "twitterBotEnabled": true,
+        "twitterBotName": twitterBotName
+    };
+    $.ajax
+    ({
+        type: "POST",
+        url: queryUrl,
+        async: false,
+        data: JSON.stringify(data),
+        contentType: "application/json",
+        beforeSend: function (xhr) {
+            // set header if JWT is set
+            if (sessionStorage.getItem("token")) {
+                xhr.setRequestHeader("Authorization", "Bearer " + sessionStorage.getItem("token"));
+            }
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        success: function (data) {
+            alert("Twitter notificator created!");
+        },
+        error: function (data) {
+            alert(JSON.stringify(data));
+        }
+    });
 }
 
-function updateTwitterBot() {
+function updateTwitterBot(twitterBotName, twitterBotDescription, twitterBotEnabled, twitterBotConsumerKey, twitterBotConsumerSecret, twitterBotAccessToken, twitterBotAccessTokenSecret) {
+    var queryUrl = location.protocol + '//' + document.domain + ":8080/gamecrafttwitternotificationmanager/api/twitter-bots/";
 
+    var data = {
+        "id": twitterBotId,
+        "twitterBotAccessToken": twitterBotAccessToken,
+        "twitterBotAccessTokenSecret": twitterBotAccessTokenSecret,
+        "twitterBotConsumerKey": twitterBotConsumerKey,
+        "twitterBotConsumerSecret": twitterBotConsumerSecret,
+        "twitterBotDescription": twitterBotDescription,
+        "twitterBotEnabled": twitterBotEnabled,
+        "twitterBotName": twitterBotName
+    };
+    $.ajax
+    ({
+        type: "PUT",
+        url: queryUrl,
+        async: false,
+        data: JSON.stringify(data),
+        contentType: "application/json",
+        beforeSend: function (xhr) {
+            // set header if JWT is set
+            if (sessionStorage.getItem("token")) {
+                xhr.setRequestHeader("Authorization", "Bearer " + sessionStorage.getItem("token"));
+            }
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        success: function (data) {
+            alert("Twitter notificator created!");
+        },
+        error: function (data) {
+            alert(JSON.stringify(data));
+        }
+    });
 }
 
 function deleteTwitterBot() {
+    var queryUrl = location.protocol + '//' + document.domain + ":8080/gamecrafttwitternotificationmanager/api/twitter-bots/" + twitterBotId;
 
+    $.ajax
+    ({
+        type: "DELETE",
+        url: queryUrl,
+        async: false,
+        contentType: "application/json",
+        beforeSend: function (xhr) {
+            // set header if JWT is set
+            if (sessionStorage.getItem("token")) {
+                xhr.setRequestHeader("Authorization", "Bearer " + sessionStorage.getItem("token"));
+            }
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        success: function (data) {
+            alert("Twitter notificator deleted!");
+        },
+        error: function (data) {
+            alert(JSON.stringify(data));
+        }
+    });
 }
