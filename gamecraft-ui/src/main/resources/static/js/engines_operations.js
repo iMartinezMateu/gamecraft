be74df535beffd169270b6cf9c980a3f99647b42
Iván Martínez Mateu <ivanmartinezmateu@icloud.com>
2018-06-16 18:41:13 +0200

    Add CRUD functions for engines management in Gamecraft UI

--- gamecraft-ui/src/main/resources/static/js/engines_operations.js
+++ gamecraft-ui/src/main/resources/static/js/engines_operations.js
@@ -23,14 +23,98 @@ function getEngines() {
     return enginesList;
 }
 
-function addEngine() {
+function addEngine(engineName, engineDescription, engineCompilerPath, engineCompilerArguments) {
+    var queryUrl = location.protocol + '//' + document.domain + ":8080/gamecraftenginemanager/api/engines/";
 
+    var data = {
+        engineCompilerArguments: engineCompilerArguments,
+        engineCompilerPath: engineCompilerPath,
+        engineDescription: engineDescription,
+        engineName: engineName
+    };
+    $.ajax
+    ({
+        type: "POST",
+        url: queryUrl,
+        async: false,
+        data: JSON.stringify(data),
+        contentType: "application/json",
+        beforeSend: function (xhr) {
+            // set header if JWT is set
+            if (sessionStorage.getItem("token")) {
+                xhr.setRequestHeader("Authorization", "Bearer " + sessionStorage.getItem("token"));
+            }
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        success: function (data) {
+            alert("Engine created!");
+        },
+        error: function (data) {
+            alert(JSON.stringify(data));
+        }
+    });
 }
 
-function updateEngine() {
+function updateEngine(engineId, engineName, engineDescription, engineCompilerPath, engineCompilerArguments) {
+    var queryUrl = location.protocol + '//' + document.domain + ":8080/gamecraftenginemanager/api/engines/";
 
+    var data = {
+        id: engineId,
+        engineCompilerArguments: engineCompilerArguments,
+        engineCompilerPath: engineCompilerPath,
+        engineDescription: engineDescription,
+        engineName: engineName
+    };
+    $.ajax
+    ({
+        type: "PUT",
+        url: queryUrl,
+        async: false,
+        data: JSON.stringify(data),
+        contentType: "application/json",
+        beforeSend: function (xhr) {
+            // set header if JWT is set
+            if (sessionStorage.getItem("token")) {
+                xhr.setRequestHeader("Authorization", "Bearer " + sessionStorage.getItem("token"));
+            }
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        success: function (data) {
+            alert("Engine updated!");
+        },
+        error: function (data) {
+            alert(JSON.stringify(data));
+        }
+    });
 }
 
-function deleteEngine() {
+function deleteEngine(engineId) {
+    var queryUrl = location.protocol + '//' + document.domain + ":8080/gamecraftenginemanager/api/engines/" + engineId;
 
+    $.ajax
+    ({
+        type: "DELETE",
+        url: queryUrl,
+        async: false,
+        contentType: "application/json",
+        beforeSend: function (xhr) {
+            // set header if JWT is set
+            if (sessionStorage.getItem("token")) {
+                xhr.setRequestHeader("Authorization", "Bearer " + sessionStorage.getItem("token"));
+            }
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        success: function (data) {
+            alert("Engine deleted!");
+        },
+        error: function (data) {
+            alert(JSON.stringify(data));
+        }
+    });
 }
