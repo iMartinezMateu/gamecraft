c470cbebc125c7a82e0d6b1986cdf5a768ad8655
Ivan Martinez Mateu <imm@ivanmartinezmateu.com>
2018-09-15 00:23:02 +0200

    Add execute function in Gamecraft UI

--- gamecraft-ui/src/main/resources/static/js/pipeline_operations.js
+++ gamecraft-ui/src/main/resources/static/js/pipeline_operations.js
@@ -167,6 +167,33 @@ function deletePipeline(pipelineId) {
     });
 }
 
+function executePipeline(pipelineId) {
+    var queryUrl = location.protocol + '//' + document.domain + ":8080/gamecraftpipelinemanager/api/pipelines/" + pipelineId + "/execute";
+
+    $.ajax
+    ({
+        type: "GET",
+        url: queryUrl,
+        async: false,
+        contentType: "application/json",
+        beforeSend: function (xhr) {
+            // set header if JWT is set
+            if (sessionStorage.getItem("token")) {
+                xhr.setRequestHeader("Authorization", "Bearer " + sessionStorage.getItem("token"));
+            }
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        success: function (data) {
+            alert("Pipeline executed!");
+        },
+        error: function (data) {
+            alert(JSON.stringify(data));
+        }
+    });
+}
+
 function deletePipelinesAssociatedToProject(projectId) {
 
     var pipelines = getPipelines();
